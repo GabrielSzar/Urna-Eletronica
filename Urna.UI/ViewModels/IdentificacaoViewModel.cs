9b75e9a3fc00a8e9fb6d129902aae64c1f0a1329
IgorDominguez <igordominguez0079080@gmail.com>
2026-09-24 06:13:08 -0300

    feat: Adição inicial da tela de login

--- Urna.UI/ViewModels/IdentificacaoViewModel.cs
+++ Urna.UI/ViewModels/IdentificacaoViewModel.cs
@@ -1,21 +1,54 @@
+using System.Linq;
+using Avalonia.Media;
+using CommunityToolkit.Mvvm.ComponentModel;
 using CommunityToolkit.Mvvm.Input;
 using Urna.UI.Services;
 namespace Urna.UI.ViewModels;
 
 public partial class IdentificacaoViewModel : ViewModelBase
 {
-    private readonly INavigationService _navigationService;
-    private readonly VotacaoViewModel _votacaoViewModel;
-
-    public IdentificacaoViewModel(INavigationService navigationService, VotacaoViewModel votacaoViewModel)
-    {
-        _navigationService = navigationService;
-        _votacaoViewModel = votacaoViewModel;
-    }
+    [ObservableProperty] public string mensagemMatricula = string.Empty;
+    [ObservableProperty] public IBrush corMensagemMatricula = Brushes.Transparent;
 
     [RelayCommand]
-    private void VerificarMatricula()
+    public void ValidarMatricula(string matricula)
     {
-        _navigationService.CurrentViewModel = _votacaoViewModel;
+        if (string.IsNullOrEmpty(matricula))
+        {
+            MensagemMatricula = "A matrícula deve ter 8 caracteres";
+            CorMensagemMatricula = Brushes.Red;
+            return;
+        }
+
+        if (matricula.Length > 8)
+        {
+            MensagemMatricula = "A matrícula deve ter somente 8 caracteres";
+            CorMensagemMatricula = Brushes.Red;
+            return;
+        }
+
+        if (matricula.Length < 8)
+        {
+            MensagemMatricula = "A matrícula deve ter 8 caracteres";
+            CorMensagemMatricula = Brushes.Red;
+            return;
+        }
+
+        if (!matricula.All(char.IsDigit))
+        {
+            MensagemMatricula = "A matrícula deve somente números";
+            CorMensagemMatricula = Brushes.Red;
+            return;
+        }
+
+        if (!matricula.StartsWith("1250"))
+        {
+            MensagemMatricula = "Matrícula inválida!";
+            CorMensagemMatricula = Brushes.Red;
+            return;
+        }
+
+        MensagemMatricula = "Matrícula válida!";
+        CorMensagemMatricula = SolidColorBrush.Parse("#415158");
     }
 }
